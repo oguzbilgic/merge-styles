050e81ce74c69de7bb3fe2423cf4b97ff4204388
Oguz Bilgic <fisyonet@gmail.com>
2015-07-27 17:51:42 -0700

    Require object-assign

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-// https://speakerdeck.com/vjeux/react-css-in-js
+var assign = require('object-assign');
 
+// https://speakerdeck.com/vjeux/react-css-in-js
 module.exports = function m() {
   var res = {};
   for (var i = 0; i < arguments.length; ++i) {
     if (arguments[i]) {
-      Object.assign(res, arguments[i]);
+      assign({}, res, arguments[i]);
     }
   }
   return res;
